023bab062b0f3452bad7413030480fcad316598b
thelinuxfraud <136052607+thelinuxfraud@users.noreply.github.com>
2023-06-09 01:40:30 -0400

    Create arch-install-script.sh

---
+++ arch-install-script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#########################
+
+sudo pacman -S thunar lynis ufw arch-audit pwgen cracklib zsh zsh-syntax-highlighting neovim alacritty volumeicon network-manager-applet virt-manager qemu-desktop dnsmasq iptables-nft neovim emacs ripgrep fd flameshot htop dunst exa lf rofi khal
