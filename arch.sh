9417e911a6a2ba3e6a750bc2953da2d9cf934038
thelinuxfraud <151329771+thelinuxfraudd@users.noreply.github.com>
2023-11-23 22:16:45 -0500

    arch.sh

---
+++ arch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#########################
+
+sudo pacman -S thunar python-dbus-next obs-studio lynis ufw arch-audit pwgen cracklib zsh neovim kitty alacritty gimp kdenlive volumeicon network-manager-applet virt-manager qemu-desktop dnsmasq iptables-nft emacs ripgrep fd xfce4-screenshooter btop dunst eza rofi khal bat bat-extras discord xclip xorg-xclipboard chromium ristretto qutebrowser picom lf kvanatum fish rkhunter 
+
