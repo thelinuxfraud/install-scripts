a10bb60c1bafb4960743ef0658df93435bbb2ed8
blake <151329771+thelinuxfraud@users.noreply.github.com>
2024-03-15 10:16:54 -0400

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -2,5 +2,5 @@
 
 #########################
 
-sudo pacman -S thunar python-dbus-next obs-studio lynis ufw arch-audit pwgen cracklib zsh neovim kitty alacritty gimp kdenlive volumeicon network-manager-applet virt-manager qemu-desktop dnsmasq iptables-nft emacs ripgrep fd xfce4-screenshooter btop dunst eza rofi khal bat bat-extras discord xclip xorg-xclipboard chromium ristretto qutebrowser picom lf kvanatum fish rkhunter 
+sudo pacman -S thunar python-dbus-next obs-studio lynis ufw arch-audit pwgen cracklib zsh neovim kitty alacritty gimp kdenlive volumeicon network-manager-applet virt-manager qemu-desktop dnsmasq iptables-nft emacs ripgrep fd xfce4-screenshooter btop dunst eza rofi khal bat bat-extras discord xclip xorg-xclipboard chromium ristretto qutebrowser picom lf kvantum fish rkhunter 
 
